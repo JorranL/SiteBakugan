e56785cea2a5b525ae53825933d149aec9c47d4a
Jorran Luka <jorransantoss@gmail.com>
2026-08-19 11:44:30 -0300

    react pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Substitua 'NOME-DO-SEU-REPOSITORIO' pelo nome exato do repositório no GitHub
 export default defineConfig({
   plugins: [react()],
+  base: 'SiteBakugan', 
 })
